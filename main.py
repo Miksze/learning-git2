c40af450e78a98aa2746e42555324ad2fd3a5e12
Mikolaj Szechalewicz <mikolaj.szechalewicz@ekspertrynku.pl>
2022-09-02 21:34:49 +0200

    dodane zadanie 2

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ for x, y in (lista.items()):
 count = { k: len(v) for k, v in lista.items() }
 count2 = sum(count.values())
 print( f"W sumie kupię {count2} produktów." ) 
+# Zadanie 2
+zbior = []
+for i in range(0,100):
+    if i % 5 == 0:
+        zbior.append(i)
+print(zbior)
+x3 = [number * number * number for number in zbior]
+print(f"Te sześciany {x3}")
